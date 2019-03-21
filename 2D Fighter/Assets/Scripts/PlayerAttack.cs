0e19b47fa718910e7b67b2f15583a3d9ab788a21
ethanloewen <44653792+ethanloewen@users.noreply.github.com>
2019-03-20 19:48:15 -0700

    Added Attack and enemy heath

---
+++ 2D Fighter/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+    public int damage;
+
+    void Update()
+    {
+        if(timeBtwAttack <= 0)
+        {
+            //then u can attack
+            if (Input.GetKey(KeyCode.Space))
+            {
+                //loop through objects with layer mask "what is enemies" and loop though "enemiesToDamage" applying dmg
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<EnemyHealth>().TakeDamage(damage);
+                }
+                
+                //reset time to attack
+                timeBtwAttack = startTimeBtwAttack;
+            }
+        }
+        else
+        {
+            //subtract time from "time between attack" amount
+            timeBtwAttack -= Time.deltaTime;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+
+}
